890a8c327f9cf032140752877f73859d560a98ef
John Sylvain <john.n.sylvain@gmail.com>
2020-06-19 14:02:05 -0500

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
-type State<T> = {
+export type State<T> = {
   [K in keyof T]: T[K];
 };
 
-type SimpleActionHandler<T> = (
+export type SimpleActionHandler<T> = (
   data?: any
 ) => Partial<State<T>> | void | Promise<State<T>>;
 
-type ComplexActionHandler<T> = (
+export type ComplexActionHandler<T> = (
   data?: any
 ) => (
     state: State<T>,
     actions: MappedActions<T>
   ) => Partial<State<T>> | void | Promise<State<T>>;
 
-type Actions<T> = {
+export type Actions<T> = {
   [key: string]: SimpleActionHandler<T> | ComplexActionHandler<T>;
 };
 
-type MappedActions<T> = {
+export type MappedActions<T> = {
   [key: string]: (data?: any) => Partial<State<T>> | void | Promise<T>;
 };
 
-type SubscriptionFn<T> = (
+export type SubscriptionFn<T> = (
   state: State<T>,
   triggerAction?: keyof MappedActions<T>
 ) => void;
